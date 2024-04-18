6b47b50fc8f859280fde656cba46952aa98e3ddc
JOHN FREDDY CARVAJAL GUAJE <john.carvajal1074@correo.policia.gov.co>
2024-04-18 11:16:40 -0500

    linq platzi

---
+++ LinqQueries.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_linq
+{
+   public class LinqQueries
+{
+    private List<Book> librosCollection = new List<Book>();
+
+    public LinqQueries()
+    {
+        using(StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.librosCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() {PropertyNameCaseInsensitive = true});
+        }
+    }
+
+    public IEnumerable<Book> TodaLaColeccion()
+    {
+        return librosCollection;
+    }
+
+    public IEnumerable<Book> LibrosDespuesdel2000()
+    {
+        //extension method
+        //return librosCollection.Where(p=> p.PublishedDate.Year > 2000);
+
+        //query expresion
+
+        return from l in librosCollection where l.PublishedDate.Year > 2000 select l;
+    }
+
+    public IEnumerable<Book> LibrosConMasde250PagConPalabrasInAction()
+    {
+        //extension methods
+        //return librosCollection.Where(p=> p.PageCount > 250 && p.Title.Contains("in Action"));
+
+        //query expression
+        return from l in librosCollection where l.PageCount > 250 && l.Title.Contains("in Action") select l;
+    }
+
+    public bool TodosLosLibrosTienenStatus()
+    {
+        return librosCollection.All(p=> p.Status!= string.Empty);
+    }
+
+    public bool SiAlgunLibroFuePublicado2005()
+    {
+        return librosCollection.Any(p=> p.PublishedDate.Year == 2005);
+    }
+
+    public IEnumerable<Book> LibrosdePython()
+    {
+        return librosCollection.Where(p=> p.Categories.Contains("Python"));
+    }
+
+    public IEnumerable<Book> LibrosdeJavaPorNombreAscendente()
+    {
+        return librosCollection.Where(p=> p.Categories.Contains("Java")).OrderBy(p=> p.Title);
+    }
+
+    public IEnumerable<Book> Librosmasde450pagOrdernadorPorNumPagDescendente()
+    {
+        return librosCollection.Where(p=> p.PageCount > 450).OrderByDescending(p=> p.PageCount);
+    }
+
+    public IEnumerable<Book> TresPrimerosLibrosJavaOrdenadosPorFecha()
+    {
+        return librosCollection
+        .Where(p=> p.Categories.Contains("Java"))
+        .OrderBy(p=> p.PublishedDate)
+        .TakeLast(3);
+    }
+
+    public IEnumerable<Book> TerceryCuartoLibroDeMas400Pag()
+    {
+        return librosCollection
+        .Where(p=> p.PageCount > 400)
+        .Take(4)
+        .Skip(2);
+    }
+
+    public IEnumerable<Book> TresPrimeroLibrosDeLaCollecion()
+    {
+        return librosCollection.Take(3)
+        .Select(p=> new Book() { Title= p.Title, PageCount= p.PageCount  });
+    }
+
+    public long CantidadDeLibrosEntre200y500Pag()
+    {
+        return librosCollection.LongCount(p=> p.PageCount>=200 && p.PageCount<=500);
+    }
+
+    public DateTime FechaDePublicacionMenor()
+    {
+        return librosCollection.Min(p=> p.PublishedDate);
+    }
+
+    public int NumeroDePagLibroMayor()
+    {
+        return librosCollection.Max(p=> p.PageCount);
+    }
+
+    public Book LibroConMenorNumeroDePaginas()
+    {
+        return librosCollection.Where(p=> p.PageCount>0).MinBy(p=> p.PageCount);
+    }
+
+    public Book LibroConFechaPublicacionMasReciente()
+    {
+        return librosCollection.MaxBy(p=> p.PublishedDate);
+    }
+
+    public int SumaDeTodasLasPaginasLibrosEntre0y500()
+    {
+        return librosCollection.Where(p=> p.PageCount >= 0 && p.PageCount <=500).Sum(p=> p.PageCount);
+    }
+    
+    public string TitulosDeLibrosDespuesDel2015Concatenados()
+    {
+        return librosCollection
+                .Where(p=> p.PublishedDate.Year > 2015)
+                .Aggregate("", (TitulosLibros, next) =>
+                {
+                    if(TitulosLibros != string.Empty)
+                        TitulosLibros += " - " + next.Title;
+                    else
+                        TitulosLibros += next.Title;
+
+                    return TitulosLibros;
+                });
+    }
+
+    public double PromedioCaracteresTitulo()
+    {
+        return librosCollection.Average(p=> p.Title.Length);
+    }
+
+    public IEnumerable<IGrouping<int, Book>> LibrosDespuesdel2000AgrupadosporAno()
+    {
+        return librosCollection.Where(p=> p.PublishedDate.Year >= 2000).GroupBy(p=> p.PublishedDate.Year);
+    }
+
+    public ILookup<char, Book> DiccionariosDeLibrosPorLetra()
+    {
+        return librosCollection.ToLookup(p=> p.Title[0], p=> p);
+    }
+
+    public IEnumerable<Book> LibrosDespuesdel2005conmasde500Pags()
+    {
+        var LibrosDepuesdel2005 = librosCollection.Where(p=> p.PublishedDate.Year > 2005);
+
+        var LibrosConMasde500pag = librosCollection.Where(p=> p.PageCount > 500);
+
+        return LibrosDepuesdel2005.Join(LibrosConMasde500pag, p=> p.Title, x=> x.Title, (p, x) => p);
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using curso_linq;
+
+LinqQueries queries = new LinqQueries();
+
+//Toda la coleccion
+//ImprimirValores(queries.TodaLaColeccion());
+
+//Libros despues del 2000
+//ImprimirValores(queries.LibrosDespuesdel2000());
+
+//Libros que tienen mas de 250 pags y tienen en el titulo la palabra in action
+//ImprimirValores(queries.LibrosConMasde250PagConPalabrasInAction());
+
+//Todos los libros tienen Status
+//Console.WriteLine($" Todos los libros tienen status? - {queries.TodosLosLibrosTienenStatus()}");
+
+//Si algun libro fue publicado en 2005
+//Console.WriteLine($" Algun libro fue publicado en 2005? - {queries.SiAlgunLibroFuePublicado2005()}");
+
+
+//Libros de python
+//ImprimirValores(queries.LibrosdePython());
+
+//libros de Java ordenados por nombre
+//ImprimirValores(queries.LibrosdeJavaPorNombreAscendente());
+
+//libros que tienen mas de 450 paginas ordernados por cantidad de paginas
+//ImprimirValores(queries.Librosmasde450pagOrdernadorPorNumPagDescendente());
+
+//los 3  libros de Java publicados recientemente
+//ImprimirValores(queries.TresPrimerosLibrosJavaOrdenadosPorFecha());
+
+//tercer y caurto libro con mas de 400 paginas
+//ImprimirValores(queries.TerceryCuartoLibroDeMas400Pag());
+
+//tres primeros libros filtrados con Select
+//ImprimirValores(queries.TresPrimeroLibrosDeLaCollecion());
+
+//cantidad de libros que tienen entre 200 y 500 paginas
+//Console.WriteLine($"Cantidad de libros que tiene entre 200 y 500 pag. {queries.CantidadDeLibrosEntre200y500Pag()}");
+
+// fecha de publicacion menor de todos los libros
+//Console.WriteLine($"Fecha de publicacion menor: {queries.FechaDePublicacionMenor()}");
+
+//Numero de paginas del libro con mayor Numero de paginas
+//Console.WriteLine($"El libro con mayor numero de paginas tiene: {queries.NumeroDePagLibroMayor()} paginas. ");
+
+//Libro con menor numero de paginas
+//var libroMenorPag = queries.LibroConMenorNumeroDePaginas();
+//Console.WriteLine($"{libroMenorPag.Title} - {libroMenorPag.PageCount}");
+
+//Libro con fecha publicacion mas reciente
+//var libroFechaPubReciente = queries.LibroConFechaPublicacionMasReciente();
+//Console.WriteLine($"{libroFechaPubReciente.Title} - {libroFechaPubReciente.PublishedDate.ToShortDateString()}");
+
+//suma de paginas de libros entre 0 y 500
+//Console.WriteLine($"Suma total de paginas {queries.SumaDeTodasLasPaginasLibrosEntre0y500()}");
+
+//Libros publicados despues del 2015
+//Console.WriteLine(queries.TitulosDeLibrosDespuesDel2015Concatenados());
+
+//el promedio de caracteres del los titulos de los libros
+//Console.WriteLine($"promedio caracteres de los titulos: {queries.PromedioCaracteresTitulo()}");
+
+//Libros publicados a partir del 2000 agrupados por ano
+//ImprimirGrupo(queries.LibrosDespuesdel2000AgrupadosporAno());
+
+//Diccionario de libros agrupados por primera letra del titulo
+//var diccionarioLookup = queries.DiccionariosDeLibrosPorLetra();
+//ImprimirDiccionario(diccionarioLookup, 'Z');
+
+// libros filtradaor con la clausula join 
+ImprimirValores(queries.LibrosDespuesdel2005conmasde500Pags());
+
+
+void ImprimirValores(IEnumerable<Book> listadelibros)
+{
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach(var item in listadelibros)
+    {
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
+
+void ImprimirGrupo(IEnumerable<IGrouping<int,Book>> ListadeLibros)
+{
+    foreach(var grupo in ListadeLibros)
+    {
+        Console.WriteLine("");
+        Console.WriteLine($"Grupo: { grupo.Key }");
+        Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+        foreach(var item in grupo)
+        {
+            Console.WriteLine("{0,-60} {1, 15} {2, 15}",item.Title,item.PageCount,item.PublishedDate.Date.ToShortDateString()); 
+        }
+    }
+}
+
+void ImprimirDiccionario(ILookup<char, Book> ListadeLibros, char letra)
+{
+   Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+   foreach(var item in ListadeLibros[letra])
+   {
+         Console.WriteLine("{0,-60} {1, 15} {2, 15}",item.Title,item.PageCount,item.PublishedDate.Date.ToShortDateString()); 
+   }
+}
